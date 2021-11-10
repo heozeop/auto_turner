196fb3baa68869f7278f01e53f729be62424bf86
Lee seung chan <dltmdcks702@gmail.com>
2021-11-10 21:47:30 +0900

    feat(front): add webpage audio recording

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -17,3 +17,15 @@ async def websocket_endpoint(websocket: WebSocket):
     # websocket.receive_bytes() 로 audio byte 받을 수 있습니다. 
     data = await websocket.receive_text()
     await websocket.send_text(f"Message text was: {data}")
+
+@app.websocket("/audio")
+async def websocket_audio_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_bytes()
+            print(f"websocket receives: {data}")
+            await websocket.send_bytes(data)
+    except Exception as e:
+        print(e.message)
+
--- server/app/recordHtml.py
+++ server/app/recordHtml.py
@@ -5,103 +5,55 @@ recordHtml = """
         <title>Chat</title>
     </head>
     <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-
-        <h1>Web Audio Stream</h1>
-        <button id="startBtn">start</button>
-        <button id="stopBtn">end</button>
-        <ul id='download'>
-        </ul>
+                        
+        <section class="main">
+            <button id="startBtn">시작</button>
+            <button id="stopBtn">끝</button>
+        </section>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-            window.onload = () => {
-  const startBtn = document.getElementById('startBtn');
-  const stopBtn = document.getElementById('stopBtn');
-  
-  let blobs;
-  let blob; // 데이터
-  let rec; // 스트림을 기반으로 동작하는 mediarecorder 객체
-  let voiceStream; // 오디오스트림
-  
-  const getAudioStreams = (voiceStream) => { // 비디오, 오디오스트림 연결
-    const context = new AudioContext();
-    const destination = context.createMediaStreamDestination();
-    let hasVoice = false;
-    
-    if (voiceStream && voiceStream.getAudioTracks().length > 0) {
-      const source2 = context.createMediaStreamSource(voiceStream);
-      const voiceGain = context.createGain();
-      voiceGain.gain.value = 0.7;
-      source2.connect(voiceGain).connect(destination);
-      hasVoice = true;
-    }
-      
-    return hasVoice ? destination.stream.getAudioTracks() : [];
-  };
- 
-  startBtn.onclick = async () => { // 녹화 시작 버튼을 누른 경우
-    
-    voiceStream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true }); // 오디오스트림 생성
-  
-    const tracks = [
-      ...getAudioStreams(voiceStream)
-    ];
-    
-    stream = new MediaStream(tracks);
-      
-    blobs = [];
- 
-    rec = new MediaRecorder(stream, {mimeType: 'video/webm; codecs=vp9,opus'}); // mediaRecorder객체 생성
-    rec.ondataavailable = (e) => blobs.push(e.data);
-    rec.onstop = async () => {
-      
-      blob = new Blob(blobs, {type: 'video/webm'});
-      let url = window.URL.createObjectURL(blob);
-      
-                var messages = document.getElementById('download')
-                var message = document.createElement('li')
-                var content = document.createTextNode(url)
-                message.appendChild(content)
-                messages.appendChild(message)
-    };
-    startBtn.disabled = true; // 시작 버튼 비활성화
-    stopBtn.disabled = false; // 종료 버튼 활성화
-    rec.start(); // 녹화 시작
-  };
- 
-  stopBtn.onclick = () => { // 종료 버튼을 누른 경우
-    // 버튼 비활성화
-    startBtn.disabled = true;
-    stopBtn.disabled = true;
-    
-    rec.stop(); // 화면녹화 종료 및 녹화된 영상 다운로드
-    
-    voiceStream.getTracks().forEach(s=>s.stop())
-    voiceStream = null;
- 
-    startBtn.disabled = false; // 시작 버튼 활성화
-  };
-};
+            const audioContext = new window.AudioContext();
+            let voiceStream;
+            let ws;
+            let rec;
+            
+            const startBtn = document.getElementById('startBtn');
+            const stopBtn = document.getElementById('stopBtn');
 
+            window.onload = () => {
+                ws = new WebSocket("ws://localhost:8000/audio");
+                ws.onopen = function() {
+                    console.log('Connected');
+                };
+
+                startBtn.onclick = async () => {
+                    voiceStream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true }); // 오디오스트림 생성
+                    
+                    rec = new MediaRecorder(voiceStream, {mimeType: 'audio/webm'}); 
+
+                    rec.ondataavailable = (e) => {
+                        console.log('receiving', e.data);
+                        ws.send(e.data);
+                    }
+                    rec.onstop = async () => {
+                        console.log("stoped");
+                    };
+                    startBtn.disabled = true;
+                    stopBtn.disabled = false; 
+                    rec.start();
+                };
+            
+                stopBtn.onclick = () => {
+                    startBtn.disabled = true;
+                    stopBtn.disabled = true;
+                    
+                    rec.stop();
+                    
+                    voiceStream.getTracks().forEach(s=>s.stop())
+                    voiceStream = null;
+                
+                    startBtn.disabled = false;
+                };
+            }
         </script>
     </body>
 </html>
