e625bb711c053fa7b92ea8624acce97829d96f04
SouthColumn76 <nrkim01@gmail.com>
2021-11-25 13:11:54 +0900

    simple websocket test

---
+++ android/app/src/main/java/com/example/auto_turner/SocketAdapter.java
@@ -0,0 +1,13 @@
+package com.example.auto_turner;
+
+import android.util.Log;
+
+import com.neovisionaries.ws.client.WebSocket;
+import com.neovisionaries.ws.client.WebSocketAdapter;
+
+public class SoketAdapter extends WebSocketAdapter {
+    @Override
+    public void onTextMessage(WebSocket websocket, String text) throws Exception {
+        Log.d("TAG", "onTextMessage: " + text);
+    }
+}
