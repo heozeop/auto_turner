8776649dbd4ed7597c822d8401dc9003ac5fc222
SouthColumn76 <nrkim01@gmail.com>
2021-12-05 21:10:27 +0900

    checking new note

--- android/app/src/main/java/com/example/auto_turner/MelodyAnalyser.java
+++ android/app/src/main/java/com/example/auto_turner/MelodyAnalyser.java
@@ -10,35 +10,35 @@ import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class MelodyAnalyser {
-    private final Queue<JSONArray> queue = new LinkedBlockingQueue<>();
+    private Queue<Note> queue = null;
     private JSONArray lastData = new JSONArray();
 
-    public int length(){
-        int length = queue.size();
-        queue.clear();
-        return length;
+    public MelodyAnalyser(Queue<Note> quere){
+        this.queue = quere;
     }
 
     public void recvJson(JSONArray array) {
+        Note frame = new Note();
         try{
             for (int i =0; i < array.length(); i++) {
                 JSONObject object = array.getJSONObject(i);
-                if (isNew(object.getInt("note"), object.getInt("pitch"))){
-
-                }
+                int note = object.getInt("note"), pitch = object.getInt("pitch");
+                if (isNew(note, pitch))
+                    frame.add(note, pitch);
             }
         } catch (JSONException e){
             e.printStackTrace();
         }
         lastData = array;
-        
+        queue.add(frame);
     }
 
-    private boolean checkDiff(JSONArray array){
-        return !array.equals(lastData);
-    }
-    private boolean isNew(int note, int pitch){
-
-        return false;
+    private boolean isNew(int note, int pitch) throws JSONException{
+        for (int i = 0; i < lastData.length(); i++){
+            JSONObject object = lastData.getJSONObject(i);
+            if (note == object.getInt("note") && pitch == object.getInt("pitch"))
+                return false;
+        }
+        return true;
     }
 }
