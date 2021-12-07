abb1f56b03e4831cada2861332c5399479d5d9d2
SouthColumn76 <nrkim01@gmail.com>
2021-12-07 22:39:47 +0900

    refactor FFTClient

--- android/app/src/main/java/com/example/auto_turner/FFTClient.java
+++ android/app/src/main/java/com/example/auto_turner/FFTClient.java
@@ -4,56 +4,43 @@ import com.neovisionaries.ws.client.WebSocket;
 import com.neovisionaries.ws.client.WebSocketAdapter;
 import com.neovisionaries.ws.client.WebSocketFactory;
 
-import java.io.OutputStream;
-
 public class FFTClient {
     private WebSocket ws = null;
-    private WebSocketAdapter adapter = null;
+    private WebSocketAdapter adapter;
     private String host = null;
 
-    public FFTClient(WebSocketAdapter adapter){
+    public FFTClient(WebSocketAdapter adapter) {
         this.adapter = adapter;
     }
 
-    public void setHost(String host){
+    public void setHost(String host) {
         this.host = host;
     }
 
-    public void connect() throws Exception {
-        this.connect(this.host, 8000);
+    public void connect() {
+        try{
+            this.connect(this.host, 8000);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public void connect(String host, int timeout) throws Exception{
+    public void connect(String host, int timeout) throws Exception {
         WebSocketFactory factory = new WebSocketFactory().setConnectionTimeout(timeout);
         ws = factory.createSocket(host);
         ws.addListener(adapter);
         ws.connectAsynchronously();
     }
 
-    public void disconnect(){
+    public void disconnect() {
         if(ws.isOpen()){
             ws.disconnect();
             ws = null;
         }
     }
 
-    public void sendMessage(){
-        if(ws.isOpen()){
-            ws.sendText("Message from Android");
-        }
-    }
-
-    public void sendBytes(byte[] bytes){
-        if(ws.isOpen()){
+    public void sendBytes(byte[] bytes) {
+        if(ws.isOpen())
             ws.sendBinary(bytes);
-        }
-    }
-
-    public void loadSheet(){
-        try{
-            this.connect();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
     }
 }
