cb9fe5755a80396529134212ee26c49a94187108
SouthColumn76 <nrkim01@gmail.com>
2021-12-07 21:01:44 +0900

    Rule functions add

--- android/app/src/main/java/com/example/auto_turner/Rule.java
+++ android/app/src/main/java/com/example/auto_turner/Rule.java
@@ -1,2 +1,9 @@
-package com.example.auto_turner;public interface Rule {
+package com.example.auto_turner;
+
+import java.util.ArrayList;
+
+public interface Rule {
+    public boolean check(Note note);
+    public int next();
+    public void serBar(ArrayList<Note> bar);
 }
