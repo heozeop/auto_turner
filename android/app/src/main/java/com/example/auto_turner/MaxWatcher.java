8f813b1d54c1eec60ea4774510f6638b7617dcb5
JoonHo <junohb135@gmail.com>
2021-12-07 22:09:49 +0900

    Add Min Max func

---
+++ android/app/src/main/java/com/example/auto_turner/MaxWatcher.java
@@ -0,0 +1,44 @@
+package com.example.auto_turner;
+
+import java.util.ArrayList;
+
+public class MaxWatcher implements Rule {
+    public ArrayList<Note> bar;
+    public int index;
+    Note note; //연주중인 노트
+    Note checkNote; //검사하기 위한 노트
+    private boolean isHave;
+    @Override
+    public boolean check(Note note) {
+        if(isHave) {
+            return false;
+        }
+        else {
+            for(int i=0;i<checkNote.length();i++) {
+                if(checkNote.getNote(i)==note.getNote(0))
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public int next() {
+        index=index+1;
+        return index;
+    }
+
+    public boolean isThere() {
+        for(int i=0; i<bar.size();i++) {
+            return bar.get(i).getNote(0)==note.getNote(0);
+        }
+        return false;
+    }
+
+
+    @Override
+    public void setBar(ArrayList<Note> nextBar) {
+        bar=nextBar;
+        isHave=isThere();
+    }
+}
